8ef4ea43cc67977584ceb664bb40a25c4a6d23f6
Merkurev Sergei <MerkurevSergei@yandex.ru>
2024-07-14 00:08:29 +0900

    [PGEXREP-1] Добавлен Spring Boot и Gradle

---
+++ src/test/java/com/github/merkurevsergei/pgexrep/PgexrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.merkurevsergei.pgexrep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgexrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
